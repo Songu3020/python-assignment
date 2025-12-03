f14695e91d37e8eef767db9a66aa2a9421c1efcb
Songu3020 <petersongu2000@gmail.com>
2025-12-03 08:04:35 +0100

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,14 @@
+BMI= weight*height
+weight = float(input("enter your weight in kilograms(kg):")
+height = float("input("enter your height in meters(m)"))
+if height <=0 or weight<=0:
+print("Weight and height as positive values.")
+else
+bmi = weight/(height**2)
+print the result format into three decimal places
+print("weight - height")*30)
+print(calculated BMI is:(bmi:.2f")
+print("weight - height")*30)
+BMI is = weight(kg)/Height(m)
+the BMI will be 30/30.1
+
