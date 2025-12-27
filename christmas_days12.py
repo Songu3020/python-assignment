ea83647f65483628e70922a65f89797c076edc62
Songu3020 <petersongu2000@gmail.com>
2025-12-27 19:32:39 +0100

    Pizza and Xmas

---
+++ christmas_days12.py
@@ -0,0 +1,107 @@
+def christmas_days12():
+
+ for number in range (1,13):
+    
+    match number: 
+    
+       case 1:
+
+         print("first", end="")
+
+       case 2:
+
+         print("second", end="")
+
+       case 3:
+
+         print("Third", end="")
+
+       case 4:
+
+         print("Fouth", end="")
+
+       case 5:
+
+         print("Fifth", end="")
+
+       case 6:
+
+         print("sixth", end="")
+
+       case 7:
+
+         print("seventh", end="")
+
+       case 8:
+
+         print("eight", end="")
+
+       case 9:
+
+         print("nineth", end="")
+
+       case 10:
+
+         print("Tenth", end="")
+
+       case 11:
+
+         print("Eleventh", end="")
+       case 12:
+
+         print("Twelveth", end="")
+    
+    def switch: ()
+    
+    case 12:
+
+         print("Twelve Drummers Drumming")
+
+    case 11:
+
+         print("Eleven Pipers Piping")
+
+    case10:
+
+         print("Ten Lords a-Leapin")
+
+    case 9:
+
+         print("Nine Ladies Dancing")
+
+    case 8:
+
+         print("Eight Maids a-Milking")
+
+    case 7:
+
+         print("Seven Swans a-Swimming")
+
+    case 6:
+
+         print("Six Geese a-Laying")
+
+    case 5:
+
+         print("Five golden rings")
+
+    case 4:
+
+         print("Four Calling Birds")
+
+    case 3:
+
+         print("Three French Hens")
+
+    case 2:
+
+         print("Two Turtle Doves")
+    case 1:
+
+         print("A Partridge in a Pear Tree")
+    
+         print("Day of Christmas")
+
+
+ 
+
